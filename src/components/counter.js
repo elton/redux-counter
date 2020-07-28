f9e92f39eee3d9a7077c0d187190979e836ef792
Elton Zheng <elton.zheng.sh@gmail.com>
2020-07-28 21:41:49 +0800

    A simple counter Redux sample.

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,7 +1,13 @@
 import React from "react";
 
-function Counter() {
-  return <div>Hello, World!</div>;
+function Counter({ value, onIncrement, onDecrement }) {
+  console.log(value);
+  return (
+    <div>
+      Clicked: {value} times <button onClick={onIncrement}>+</button>
+      <button onClick={onDecrement}>-</button>
+    </div>
+  );
 }
 
 export default Counter;
--- src/index.js
+++ src/index.js
@@ -2,5 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import Counter from "./components/counter";
+import counter from "./reducers";
+import { createStore } from "redux";
 
-ReactDOM.render(<Counter />, document.getElementById("root"));
+const store = createStore(counter);
+
+const render = () =>
+  ReactDOM.render(
+    <Counter
+      value={store.getState()}
+      onIncrement={() => store.dispatch({ type: "INCREMENT" })}
+      onDecrement={() => store.dispatch({ type: "DECREMENT" })}
+    />, // 组建的参数是以对象方式传递的，所以counter函数的形参需要以counter({value, onIncrement, onDecrement})的方式书写。
+    document.getElementById("root")
+  );
+
+render();
+store.subscribe(render);
